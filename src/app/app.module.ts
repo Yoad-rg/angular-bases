2d1a84d128f88249cb4cfe21b6f88c325b217188
Yolanda Rossi Garcia <alegra.rg@gmail.com>
2024-08-08 11:25:37 +0200

    actualizando a mitad de seccion 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { CounterModule } from './counter/counter.module';
 import { HeroesModule} from './heroes/heroes.module';
-
+import { DbzModule } from './dbz/dbz.module';
 
 
 
 import { AppComponent } from './app.component';
+
 
 
 
@@ -23,6 +25,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     CounterModule,
     HeroesModule,
+    DbzModule
+
 
 
   ],
---
+++ src/app/dbz/pages/main-page.component.ts
@@ -0,0 +1,11 @@
+import {
+ Component} from '@angular/core';
+
+@Component({
+  selector: 'dbz-main-page',
+  templateUrl: './main-page.component.html'
+})
+
+export class MainPageComponent {
+
+}
