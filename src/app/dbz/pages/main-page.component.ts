41ea746e32a1fe56128cc10a02f50ed3d5c18c23
Yolanda Rossi Garcia <alegra.rg@gmail.com>
2024-08-10 12:08:31 +0200

    fin seccion 5

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component} from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -8,26 +9,17 @@ import { Character } from '../interfaces/character.interface';
 
 export class MainPageComponent {
 
-  public characters: Character[] = [{
-    name: 'Krillin',
-    power: 1000
-  },{
-    name: 'Goku',
-    power: 9500
-  },
-  {
-    name: 'Vegeta',
-    power: 7500
+  constructor(private dbzService:DbzService) {}
+
+  get characters(): Character[] {
+    return [...this.dbzService.characters];
   }
-];
 
-onNewCharacter(character: Character):void{
-  this.characters.push(character);
+  onDeleteCharacter(id:string):void{
+    this.dbzService.deleteCharacterById(id);
+  }
 
-}
-
-onDeleteCharacter(index:number) {
-  this.characters.splice(index, 1);
-}
-
+  onNewCharacter(character: Character):void{
+    this.dbzService.addCharacter(character);
+  }
 }
