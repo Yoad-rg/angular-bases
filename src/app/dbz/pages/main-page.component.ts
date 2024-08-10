f0398d9efcd1c77d13015d8045f6ac941b2fa83d
Yolanda Rossi Garcia <alegra.rg@gmail.com>
2024-08-10 10:37:37 +0200

    solucion tarea flujo completo

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,5 +1,5 @@
-import {
- Component} from '@angular/core';
+import { Component} from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -7,5 +7,27 @@ import {
 })
 
 export class MainPageComponent {
+
+  public characters: Character[] = [{
+    name: 'Krillin',
+    power: 1000
+  },{
+    name: 'Goku',
+    power: 9500
+  },
+  {
+    name: 'Vegeta',
+    power: 7500
+  }
+];
+
+onNewCharacter(character: Character):void{
+  this.characters.push(character);
+
+}
+
+onDeleteCharacter(index:number) {
+  this.characters.splice(index, 1);
+}
 
 }
